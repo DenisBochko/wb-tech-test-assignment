16040005697eb8795f3adb624b1fa9bdbb09767a
reElcy <reElcy@yandex.ru>
2025-08-22 01:44:34 +0300

    feat: add init database to main

--- cmd/wb_tech_test_assignment/main.go
+++ cmd/wb_tech_test_assignment/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"go.uber.org/zap"
 
 	"wb-tech-test-assignment/internal/config"
 	"wb-tech-test-assignment/pkg/logger"
+	"wb-tech-test-assignment/pkg/postgres"
 )
 
 func main() {
+	ctx := context.Background()
+
 	cfg := config.MustLoadConfig()
 	config.MustPrintConfig(cfg)
 
@@ -31,7 +35,29 @@ func main() {
 		}
 	}()
 
-	log.Debug("start")
+	postgresCfg := &postgres.Config{
+		Host:     cfg.Database.Host,
+		Port:     cfg.Database.Port,
+		User:     cfg.Database.User,
+		Password: cfg.Database.Password,
+		Name:     cfg.Database.Name,
+		SSLMode:  cfg.Database.SSLMode,
+		MaxConns: cfg.Database.MaxConns,
+		MinConns: cfg.Database.MinConns,
+		Migration: postgres.Migration{
+			Path:      cfg.Migration.Path,
+			AutoApply: cfg.Migration.AutoApply,
+		},
+	}
+
+	database, err := postgres.New(postgresCfg)
+	if err != nil {
+		log.Fatal("failed to connect to database", zap.Error(err))
+	}
+
+	defer database.Close()
+
+	log.Debug("start", zap.Any("database", database.Pool().Ping(ctx)))
 	log.Info("start")
 	log.Warn("start")
 
