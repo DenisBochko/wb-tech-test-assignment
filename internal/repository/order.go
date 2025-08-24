fa17a0d79a0bde85d7ae59a26957a4cbd8be9227
reElcy <reElcy@yandex.ru>
2025-08-25 00:24:13 +0300

    feat: add order repo

---
+++ internal/repository/order.go
@@ -0,0 +1,179 @@
+package repository
+
+import (
+	"context"
+	"wb-tech-test-assignment/internal/model"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type OrderRepository struct {
+	DB *pgxpool.Pool
+}
+
+func NewOrderRepository(db *pgxpool.Pool) *OrderRepository {
+	return &OrderRepository{
+		DB: db,
+	}
+}
+
+func (o *OrderRepository) InsertOrder(ctx context.Context, ext RepoExtension, order model.Order) error {
+	if ext == nil {
+		ext = o.DB
+	}
+
+	const query = `
+		INSERT INTO orders (order_uid, 
+		                    track_number, 
+		                    entry, 
+		                    locale, 
+		                    internal_signature, 
+		                    customer_id, 
+		                    delivery_service, 
+		                    shardkey, 
+		                    sm_id, 
+		                    date_created, 
+		                    oof_shard)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);
+	`
+
+	_, err := ext.Exec(ctx, query,
+		order.OrderUID,
+		order.TrackNumber,
+		order.Entry,
+		order.Locale,
+		order.InternalSignature,
+		order.CustomerID,
+		order.DeliveryService,
+		order.ShardKey,
+		order.SmID,
+		order.DateCreated,
+		order.OofShard,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (o *OrderRepository) InsertDelivery(ctx context.Context, ext RepoExtension, orderUID string, delivery model.Delivery) error {
+	if ext == nil {
+		ext = o.DB
+	}
+
+	const query = `
+		INSERT INTO deliveries (order_uid, name, phone, zip, city, address, region, email)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
+	`
+
+	_, err := ext.Exec(ctx, query,
+		orderUID,
+		delivery.Name,
+		delivery.Phone,
+		delivery.Zip,
+		delivery.City,
+		delivery.Address,
+		delivery.Region,
+		delivery.Email,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (o *OrderRepository) InsertPayment(ctx context.Context, ext RepoExtension, orderUID string, payment model.Payment) error {
+	if ext == nil {
+		ext = o.DB
+	}
+
+	const query = `
+		INSERT INTO payments (order_uid, 
+		                      transaction, 
+		                      request_id, 
+		                      currency, 
+		                      provider, 
+		                      amount, 
+		                      payment_dt, 
+		                      bank, 
+		                      delivery_cost, 
+		                      goods_total,
+		                      custom_fee)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);
+	`
+
+	_, err := ext.Exec(ctx, query,
+		orderUID,
+		payment.Transaction,
+		payment.RequestID,
+		payment.Currency,
+		payment.Provider,
+		payment.Amount,
+		payment.PaymentDt,
+		payment.Bank,
+		payment.DeliveryCost,
+		payment.GoodsTotal,
+		payment.CustomFee,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (o *OrderRepository) InsertItems(ctx context.Context, ext RepoExtension, orderUID string, items []model.Item) error {
+	if ext == nil {
+		ext = o.DB
+	}
+
+	const query = `
+		INSERT INTO items (order_uid, 
+		                   chrt_id, 
+		                   track_number, 
+		                   price, 
+		                   rid, 
+		                   name, 
+		                   sale, 
+		                   size, 
+		                   total_price, 
+		                   nm_id, 
+		                   brand, 
+		                   status) 
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);
+    `
+
+	batch := &pgx.Batch{}
+	for _, v := range items {
+		batch.Queue(query,
+			orderUID,
+			v.ChrtID,
+			v.TrackNumber,
+			v.Price,
+			v.RID,
+			v.Name,
+			v.Sale,
+			v.Size,
+			v.TotalPrice,
+			v.NmID,
+			v.Brand,
+			v.Status,
+		)
+	}
+
+	br := ext.SendBatch(ctx, batch)
+	defer func() {
+		_ = br.Close()
+	}()
+
+	for range items {
+		if _, err := br.Exec(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ internal/repository/repository.go
@@ -0,0 +1,14 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+type RepoExtension interface {
+	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
+	Exec(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error)
+	SendBatch(ctx context.Context, b *pgx.Batch) pgx.BatchResults
+}
