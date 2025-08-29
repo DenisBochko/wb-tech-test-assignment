fa873778416e2e2fb4548e60f5fcaefc7900dc41
reElcy <reElcy@yandex.ru>
2025-08-30 00:43:15 +0300

    feat: add select order to repository

--- internal/repository/order.go
+++ internal/repository/order.go
@@ -2,11 +2,16 @@ package repository
 
 import (
 	"context"
+	"errors"
+	"wb-tech-test-assignment/internal/apperrors"
 	"wb-tech-test-assignment/internal/model"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
+
+// Пояснение: Можно было бы сделать 2 запроса SELECT в бд, но т.к. полей в таблицах довольно много
+// решил разнести это по отдельным запросам.
 
 type OrderRepository struct {
 	DB *pgxpool.Pool
@@ -58,6 +63,43 @@ func (o *OrderRepository) InsertOrder(ctx context.Context, ext RepoExtension, or
 	return nil
 }
 
+func (o *OrderRepository) SelectOrder(ctx context.Context, ext RepoExtension, OrderUID string) (model.Order, error) {
+	if ext == nil {
+		ext = o.DB
+	}
+
+	const query = `
+		SELECT order_uid, track_number, entry, locale, internal_signature, customer_id, delivery_service, shardkey, sm_id, date_created, oof_shard
+		FROM orders
+		WHERE order_uid = $1;
+	`
+
+	var order model.Order
+
+	err := ext.QueryRow(ctx, query, OrderUID).Scan(
+		&order.OrderUID,
+		&order.TrackNumber,
+		&order.Entry,
+		&order.Locale,
+		&order.InternalSignature,
+		&order.CustomerID,
+		&order.DeliveryService,
+		&order.ShardKey,
+		&order.SmID,
+		&order.DateCreated,
+		&order.OofShard,
+	)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return model.Order{}, apperrors.ErrOrderNotFound
+		}
+
+		return model.Order{}, err
+	}
+
+	return order, nil
+}
+
 func (o *OrderRepository) InsertDelivery(ctx context.Context, ext RepoExtension, orderUID string, delivery model.Delivery) error {
 	if ext == nil {
 		ext = o.DB
@@ -83,6 +125,35 @@ func (o *OrderRepository) InsertDelivery(ctx context.Context, ext RepoExtension,
 	}
 
 	return nil
+}
+
+func (o *OrderRepository) SelectDelivery(ctx context.Context, ext RepoExtension, orderUID string) (model.Delivery, error) {
+	if ext == nil {
+		ext = o.DB
+	}
+
+	const query = `
+		SELECT name, phone, zip, city, address, region, email
+		FROM deliveries
+		WHERE order_uid = $1;
+	`
+
+	var delivery model.Delivery
+
+	err := ext.QueryRow(ctx, query, orderUID).Scan(
+		&delivery.Name,
+		&delivery.Phone,
+		&delivery.Zip,
+		&delivery.City,
+		&delivery.Address,
+		&delivery.Region,
+		&delivery.Email,
+	)
+	if err != nil {
+		return model.Delivery{}, err
+	}
+
+	return delivery, nil
 }
 
 func (o *OrderRepository) InsertPayment(ctx context.Context, ext RepoExtension, orderUID string, payment model.Payment) error {
@@ -123,6 +194,38 @@ func (o *OrderRepository) InsertPayment(ctx context.Context, ext RepoExtension, 
 	}
 
 	return nil
+}
+
+func (o *OrderRepository) SelectPayment(ctx context.Context, ext RepoExtension, orderUID string) (model.Payment, error) {
+	if ext == nil {
+		ext = o.DB
+	}
+
+	const query = `
+		SELECT transaction, request_id, currency, provider, amount, payment_dt, bank, delivery_cost, goods_total, custom_fee
+		FROM payments
+		WHERE order_uid = $1;
+	`
+
+	var payment model.Payment
+
+	err := ext.QueryRow(ctx, query, orderUID).Scan(
+		&payment.Transaction,
+		&payment.RequestID,
+		&payment.Currency,
+		&payment.Provider,
+		&payment.Amount,
+		&payment.PaymentDt,
+		&payment.Bank,
+		&payment.DeliveryCost,
+		&payment.GoodsTotal,
+		&payment.CustomFee,
+	)
+	if err != nil {
+		return model.Payment{}, err
+	}
+
+	return payment, nil
 }
 
 func (o *OrderRepository) InsertItems(ctx context.Context, ext RepoExtension, orderUID string, items []model.Item) error {
@@ -176,4 +279,54 @@ func (o *OrderRepository) InsertItems(ctx context.Context, ext RepoExtension, or
 	}
 
 	return nil
+}
+
+func (o *OrderRepository) SelectItems(ctx context.Context, ext RepoExtension, orderUID string) ([]model.Item, error) {
+	if ext == nil {
+		ext = o.DB
+	}
+
+	const query = `
+		SELECT chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status
+		FROM items
+		WHERE order_uid = $1;
+	`
+
+	var items []model.Item
+
+	rows, err := ext.Query(ctx, query, orderUID)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var item model.Item
+
+		err := rows.Scan(
+			&item.ChrtID,
+			&item.TrackNumber,
+			&item.Price,
+			&item.RID,
+			&item.Name,
+			&item.Sale,
+			&item.Size,
+			&item.TotalPrice,
+			&item.NmID,
+			&item.Brand,
+			&item.Status,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		items = append(items, item)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return items, nil
 }
