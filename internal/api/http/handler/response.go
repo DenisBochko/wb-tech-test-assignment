3cb48f6a8a5a79b19aea5f45b3ddbb08017de576
reElcy <reElcy@yandex.ru>
2025-08-30 00:44:35 +0300

    feat: add response with data

--- internal/api/http/handler/response.go
+++ internal/api/http/handler/response.go
@@ -2,9 +2,15 @@ package handler
 
 const (
 	statusSuccess = "success"
+	statusError   = "error"
 )
 
 type responseWithMessage struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
+}
+
+type responseWithData struct {
+	Status string      `json:"status"`
+	Data   interface{} `json:"data"`
 }
