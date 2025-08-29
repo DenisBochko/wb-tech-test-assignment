0bffccd863d6cdf688840b7199c9181e8705848a
reElcy <reElcy@yandex.ru>
2025-08-30 00:44:20 +0300

    feat: add query method to repo extension

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -11,4 +11,5 @@ type RepoExtension interface {
 	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
 	Exec(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error)
 	SendBatch(ctx context.Context, b *pgx.Batch) pgx.BatchResults
+	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
 }
