594d1193043289747eecd95e258e580acb593113
reElcy <reElcy@yandex.ru>
2025-08-30 11:31:40 +0300

    feat: add ErrShutdown

--- internal/apperrors/app_errors.go
+++ internal/apperrors/app_errors.go
@@ -4,4 +4,7 @@ import (
 	"errors"
 )
 
-var ErrOrderNotFound = errors.New("order not found")
+var (
+	ErrOrderNotFound = errors.New("order not found")
+	ErrShutdown      = errors.New("shutdown error")
+)
