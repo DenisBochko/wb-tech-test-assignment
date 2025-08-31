61fceaa46049428c1e6a807601bd009a5d1bbe04
reElcy <reElcy@yandex.ru>
2025-08-31 12:14:15 +0300

    feat: add get data script

---
+++ cmd/load_testing_script/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"wb-tech-test-assignment/internal/config"
+	"wb-tech-test-assignment/internal/model"
+)
+
+func main() {
+	cfg := config.MustLoadConfig()
+	host := cfg.HTTPServer.Host
+	port := cfg.HTTPServer.Port
+
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+		Transport: &http.Transport{
+			MaxIdleConns:        100,
+			MaxIdleConnsPerHost: 100,
+			IdleConnTimeout:     90 * time.Second,
+		},
+	}
+
+	for i := 1; i < 10000; i++ {
+		resp, err := client.Get(fmt.Sprintf("http://%s:%d/api/order/%d", host, port, i))
+		if err != nil {
+			log.Fatalf("Failed to get order %d: %v", i, err)
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		_ = resp.Body.Close()
+
+		if err != nil {
+			log.Printf("Failed to read body for order %d: %v", i, err)
+			continue
+		}
+
+		var response struct {
+			Status string      `json:"status"`
+			Order  model.Order `json:"order"`
+		}
+
+		if err := json.Unmarshal(body, &response); err != nil {
+			log.Printf("Failed to unmarshal order %d: %v", i, err)
+			continue
+		}
+
+		// печатаем ответ
+		fmt.Printf("Order %d: status=%s, order=%+v\n", i, response.Status, response.Order)
+	}
+}
